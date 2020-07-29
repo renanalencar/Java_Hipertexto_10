c986338e0ae82520bfe7a41f8fece916e4feaa28
Renan Costa Alencar <renan.costaalencar@gmail.com>
2020-07-29 15:07:06 -0300

    Correção da classe ContaCorrenteTeste.java

--- src/ContaCorrenteTeste.java
+++ src/ContaCorrenteTeste.java
@@ -9,7 +9,7 @@ public class ContaCorrenteTeste extends TestCase {
         ContaCorrente contaCorrente = new ContaCorrente("12345", 100.0);
         contaCorrente.sacar(50.0);
         double saldoCalculado = contaCorrente.saldo;
-        double saldoEsperado = 50.0;
+        double saldoEsperado = -50.0;
         double tolerancia = 0.0001;
         assertEquals(saldoEsperado, saldoCalculado, tolerancia);
     }
