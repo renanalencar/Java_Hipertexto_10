bfd867a09db3f916aabd83895201a002ccb59e18
Renan Costa Alencar <renan.costaalencar@gmail.com>
2020-07-29 15:04:27 -0300

    Meu primeiro commit

---
+++ src/ContaCorrenteTeste.java
@@ -0,0 +1,16 @@
+import org.junit.Test;
+
+import junit.framework.TestCase;
+
+public class ContaCorrenteTeste extends TestCase {
+    
+    @Test
+    public void testeSacar() {
+        ContaCorrente contaCorrente = new ContaCorrente("12345", 100.0);
+        contaCorrente.sacar(50.0);
+        double saldoCalculado = contaCorrente.saldo;
+        double saldoEsperado = 50.0;
+        double tolerancia = 0.0001;
+        assertEquals(saldoEsperado, saldoCalculado, tolerancia);
+    }
+}
